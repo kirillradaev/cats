7d4810ccd0cf290c05ee12e64c95528e7ce295c8
kirillradaev <cyrilrdv@gmail.com>
2019-08-28 01:29:27 +0000

    created tests for functions

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // pass data back via callback instead of return
+    console.log('Callback: I have the data!');
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
+  });
+};
+module.exports = breedDetailsFromFile;
